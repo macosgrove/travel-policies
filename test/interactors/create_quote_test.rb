bfc3e46c6880bddd0fea6e8e8b40895dd13422a9
macosgrove <mary-anne.cosgrove@envato.com>
2018-04-03 09:06:42 +1000

    Add test command for running all tests and Rubocop. Fix warning in create_quote_test.

--- test/interactors/create_quote_test.rb
+++ test/interactors/create_quote_test.rb
@@ -7,7 +7,7 @@ class CreateQuoteTest < ActiveSupport::TestCase
     quote = CreateQuote.call(age: 37, trip_length: 5, quote_currency: 'AUD').quote
     assert_equal 37, quote.age
     assert_equal 5, quote.trip_length
-    assert_equal nil, quote.quote_cents
+    assert_nil quote.quote_cents
     assert_equal 'AUD', quote.quote_currency
   end
 end
